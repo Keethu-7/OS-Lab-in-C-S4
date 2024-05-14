6895b704ee44f42950edaa4a9c6ff928b939d449
Keerthana J <119199296+Keethu-7@users.noreply.github.com>
2024-05-14 19:50:54 +0530

    Create fcfs.c
    First Come First Serve Scheduling in OS

---
+++ fcfs.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct fcfs {
+    int pid;
+    int btime;
+    int wtime;
+    int ttime;
+}
+p[10];
+
+int main() {
+    int i,n;
+    int totwtime=0, totttime=0;
+    printf("FCFS SCHEDULING \n");
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+    for (i=0;i<n;i++) {
+        p[i].pid=i;
+        printf("Burst time of the process %d: ",i);
+        scanf("%d",&p[i].btime);
+    }
+    p[0].wtime = 0;
+    p[0].ttime = p[0].btime;
+    for(i=0;i<n;i++) {
+        p[i].wtime = p[i-1].wtime + p[i-1].btime;
+        p[i].ttime = p[i].wtime + p[i].btime;
+        totttime += p[i].ttime;
+        totwtime += p[i].wtime;
+    }
+    printf("\n Total waiting time: %d",totwtime);
+    printf("\n Average waiting time: %f",(float)totwtime/(float)n);
+    printf("\n Total turn around time: %d",totttime);
+    printf("\n Average turn around time: %f",(float)totttime/(float)n);
+}
